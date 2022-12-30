6ebfa0f990bd53ef225c8c45f8a16a2d8d8174bb
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2022-12-30 01:09:52 +0000

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+ 
+  import Editor from "./editor/Editor.svelte"
+ 
+  let data = {
+    "items": [{
+      "id": "item-1",
+      "layout": "2-col",
+      "title": "Hello World",
+      "body": "<p>Lorem ipsum dolor site amet</p>"
+    },
+  {
+    "id": "item-2",
+    "layout": "2-col",
+    "title": "Second Item",
+    "body": "<p>Lorem ipsum dolor site lila</p>"
+  }]
+  }
+
+</script>
+
+
+<div class="container mt-5">
+
+{#each data.items as item}
+  <section id="{item.id}" class="edit">
+    <h1>{item.title}</h1>
+    <p>{@html item.body}</p>
+  </section>
+{/each}
+
+</div>
+
+
+<Editor bind:data />
+
---
+++ src/editor/Editor.svelte
@@ -0,0 +1,76 @@
+<script>
+	import { onMount } from 'svelte';
+	import { fade, fly } from 'svelte/transition';
+	import TipTap from "./TipTap.svelte"
+	
+	export let data;
+	
+	let editing = false;
+	let curIndex = false;
+	
+	onMount(async () => {
+		document.querySelectorAll('.edit').forEach((el)=>{
+		  
+		  el.addEventListener('click', (e)=>{
+			  
+			  if(editing == true){
+				  editing = false;
+			
+			  	setTimeout(()=>{
+				
+					let id = e.originalTarget.closest('.edit').getAttribute('id');
+					let index = data.items.findIndex(x => x.id == id);
+					
+					curIndex = index;
+					editing = true;
+				
+				}, 400)
+			}else{
+				let id = e.originalTarget.closest('.edit').getAttribute('id');
+				let index = data.items.findIndex(x => x.id == id);
+				
+				curIndex = index;
+				editing = true;
+			}
+		  
+		  })
+		  
+		})
+		
+	});
+	  
+	
+</script> 
+ 
+ 
+ {#if editing}
+<div class="content-editor" in:fly="{{ x: 350, duration: 700 }}" out:fly="{{ x: 350, duration: 150 }}">
+  
+ <button type="button" class="btn-close float-end mb-3" aria-label="Close" on:click={() => editing = false}></button>
+ 
+  <input type="text" class="form-control" bind:value={data.items[curIndex].title}>
+
+  
+  <TipTap bind:html={data.items[curIndex].body} />
+
+  
+  
+</div>  
+{/if}
+
+
+<style>
+	.content-editor {
+	  width: 450px;
+	  position: fixed;
+	  top: 0;
+	  right: 0;
+	  height: 100%;
+	  padding: 20px;
+	  background-color: white;
+	}
+	
+	.form-control{
+		margin-bottom: 15px;
+	}
+</style>
