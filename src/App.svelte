aadafeed5758a68c65c3373938cb162827c9e8e8
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2023-01-01 16:10:17 +0000

    Created stacking slides design

--- src/App.svelte
+++ src/App.svelte
@@ -43,6 +43,22 @@
     "subtitle": "",
     "body": "<p>Lorem ipsum dolor site lila</p>",
     "image": ""
+  },
+  {
+    "id": "item-3",
+    "component": "TwoCol",
+    "title": "Third Item",
+    "subtitle": "",
+    "body": "<p>Lorem ipsum dolor site lila</p>",
+    "image": ""
+  },
+  {
+    "id": "item-4",
+    "component": "TwoCol",
+    "title": "Fourth Item",
+    "subtitle": "",
+    "body": "<p>Lorem ipsum dolor site lila</p>",
+    "image": ""
   }]
   }
   
@@ -60,9 +76,10 @@
 <User bind:user bind:showAccount />
 
 {#if user}
-<div class="container mt-5">
+<div class="wrapper mt-5">
 
 {#each data.items as item}
+<div class="landing-card">
   {#if item.component == 'Intro'}
   <Intro bind:item={item} />
   {/if}
@@ -70,6 +87,7 @@
   {#if item.component == 'TwoCol'}
   <TwoCol bind:item={item} />
   {/if}
+</div>
 {/each}
 
 </div>
@@ -78,4 +96,33 @@
 <Editor bind:data bind:layouts bind:user bind:showAccount />
   
 {/if}
+
+
+<style>
+  
+ 
+  
+  .wrapper {
+    margin: 0 auto;
+    margin-top: 50px;
+    max-width: 768px;
+    margin-bottom: 300px;
+    padding: 15px;
+  }
+  
+  .landing-card {
+    position: sticky;
+    top: 30px;
+ color: white !important;
+    margin-bottom: 25px;
+ 
+    border-radius: 10px;
+    box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.1);
+    color: #333;
+    padding: 40px;
+    height: 400px;
+    background-color: #091a2f;
+    background-image: linear-gradient(311deg,rgba(26,188,254,.23),rgba(9,26,47,.1));
+  }
+</style>
 
