77206703d8130d9c7de2ee6414c0c1797b3a93ef
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2023-01-01 16:34:47 +0000

    Added fade-in, so you don’t see the Google sign-in every time you refresh the page

--- src/editor/User.svelte
+++ src/editor/User.svelte
@@ -75,8 +75,8 @@
 {/if}
 
 {:else}
-
-<div class="card m-auto mt-5" style="width: 18rem; color: #222">
+<div class="backdrop">
+<div class="card m-auto mt-5 fade-in" style="width: 18rem; color: #222">
  
   <div class="card-body text-center">
 	<h3 class="card-title">Sign In</h3>
@@ -87,6 +87,44 @@
 
   </div>
 </div>
-
+</div>
 
 {/if}
+
+
+<style>
+	
+	.fade-in {
+	  animation: fadeIn 5s;
+	  -webkit-animation: fadeIn 5s;
+	  -moz-animation: fadeIn 5s;
+	  -o-animation: fadeIn 5s;
+	  -ms-animation: fadeIn 5s;
+	}
+	
+	@keyframes fadeIn {
+	  0% { opacity: 0; }
+	  100% { opacity: 1; }
+	}
+	
+	@-moz-keyframes fadeIn {
+	  0% { opacity: 0; }
+	  100% { opacity: 1; }
+	}
+	
+	@-webkit-keyframes fadeIn {
+	  0% { opacity: 0; }
+	  100% { opacity: 1; }
+	}
+	
+	@-o-keyframes fadeIn {
+	  0% { opacity: 0; }
+	  100% { opacity: 1; }
+	}
+	
+	@-ms-keyframes fadeIn {
+	  0% { opacity: 0; }
+	  100% { opacity: 1; }
+	}
+	
+</style>
